3f7100b463f35bc8caa0ad8cab0cc75005e8e477
LepharamRamchiary <lepharamchiary@gmail.com>
2024-11-06 21:08:15 +0530

    view parser

--- app/Controllers/Data.php
+++ app/Controllers/Data.php
@@ -28,6 +28,39 @@ class Data extends BaseController
         $table->setTemplate($template);
 
         $records['users'] = $table->generate($data);
-        echo view('data', $records);
+        // echo view('data', $records);
+
+
+        // $parser = \Config\Services::parser();
+
+        $data2 = [
+            'title' => 'My Title',
+            'desc' => 'My Description',
+            'records' => 'My Records',
+        ];
+
+        // echo view('data', $data2);
+
+        // render method 
+        // this type is call simple substitutions
+        // return $parser->setData($data2)->render('data');
+        // echo $parser->setData($data2)->render('data');
+
+        // Loop Substitutions and Nested Substitutions
+        $parser = \Config\Services::parser();
+        $data3 = [
+            'blog_title'   => 'My Blog Title',
+            'blog_heading' => 'My Blog Heading',
+            'blog_entries' => [
+                ['title' => 'Title 1', 'body' => 'Body 1'],
+                ['title' => 'Title 2', 'body' => 'Body 2'],
+                ['title' => 'Title 3', 'body' => 'Body 3'],
+                ['title' => 'Title 4', 'body' => 'Body 4'],
+                ['title' => 'Title 5', 'body' => 'Body 5'],
+            ],
+            'status'=> false
+        ];
+
+        return $parser->setData($data3)->render('data');
     }
 }
--- app/Views/data.php
+++ app/Views/data.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users List</title>
+    <title>{blog_title}</title>
 </head>
+
 <body>
-    <h1>Users List</h1>
-    <?= $users?>
+    <!-- Loop Substitutions -->
+    <!-- <h3>{blog_heading}</h3>
+    <div class="container">
+
+        {blog_entries}
+        <h5>{title}</h5>
+        <p>{body}</p>
+        {/blog_entries}
+
+    </div> -->
+
+    <!-- Nested Substitutions -->
+    <h1>{blog_heading}</h1>
+    {blog_entries}
+    <div>
+        <h2>{title}</h2>
+        <p>{body}</p>
+    </div>
+    {/blog_entries}
+
+    {if $status}
+    <p>welcome</p>
+    {else}
+    <p>not welcomne</p>
+    {endif}
 </body>
+
 </html>
