9738754abc5f6a3e395d64d828c6e82d63fd6545
LepharamRamchiary <lepharamchiary@gmail.com>
2024-11-06 13:26:17 +0530

    remaping method

--- app/Controllers/Hi.php
+++ app/Controllers/Hi.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+// use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Hi extends BaseController
 {
@@ -8,6 +9,40 @@ class Hi extends BaseController
     {
         echo "Hello World!";
     }
-}
 
-?>
+    public function test($name)
+    {
+        echo "Hello " . $name;
+    }
+
+    public function address($city, $dist)
+    {
+        echo "I am from " . " " . $city . ", " . $dist;
+    }
+
+    // remaping
+    // public function _remap(){
+    //     echo "This is Remap";
+    // }
+
+    public function _remap($method, $paraml = null, $paraml2 = null){
+        // if($method == "test"){
+        //     return $this->test($paraml);
+        // }else if($method == "address"){
+        //     return $this->address($paraml, $paraml2);
+        // } else{
+        //     $this->index();
+        // }
+
+        if(method_exists($this, $method)){
+            return $this->$method($paraml, $paraml2);
+        }
+        else
+        {
+            return $this->index();
+        }
+
+        // Exception
+        // throw PageNotFoundException::forPageNotFound();
+    }
+}
